47b15621e524febb451612c84d37a2c7a10cc86f
josuenc <josue.naure.c@gmail.com>
2022-04-14 12:17:30 -0500

    cambios1

---
+++ javascript.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+    
+    // para debug prefiero console.log
+    //alert("Hola mundo"); 
+
+    //let varb=$('button').html(); 
+    //console.log(varb);
+    
+    //cambia el contenido de la etiqueta button
+    //$('button').html('cambiado')
+});
