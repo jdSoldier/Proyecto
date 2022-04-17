2f1dbed593e98322dfe13d11031df5c9b857771e
josuenc <josue.naure.c@gmail.com>
2022-04-17 08:53:34 -0500

    Añadi el lienzo y en el js tengo el codigo de las flechas

--- javascript.js
+++ javascript.js
@@ -1,11 +1,21 @@
-$(document).ready(function () {
-    
-    // para debug prefiero console.log
-    //alert("Hola mundo"); 
+let tecla = {
+  UP: 38,
+  DOWN: 40,
+  LEFT: 39,
+  RIGHT: 37,
+};
 
-    //let varb=$('button').html(); 
-    //console.log(varb);
-    
-    //cambia el contenido de la etiqueta button
-    //$('button').html('cambiado')
+document.addEventListener("keyup", dibujarTeclado);
+
+function dibujarTeclado(evento) {
+  console.log(evento);
+}
+
+$(document).ready(function () {
+  // para debug prefiero console.log
+  //alert("Hola mundo");
+  //let varb=$('button').html();
+  //console.log(varb);
+  //cambia el contenido de la etiqueta button
+  //$('button').html('cambiado')
 });
