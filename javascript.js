a81c05914f1755b3229c50a8fda634829ec063b8
Josue Ñaure <43712403+jdSoldier@users.noreply.github.com>
2022-04-18 22:10:26 -0500

    Se puede dibujar en el canvas
    porque puedo

--- javascript.js
+++ javascript.js
@@ -1,21 +1,63 @@
-let tecla = {
+var teclas =
+{
   UP: 38,
   DOWN: 40,
-  LEFT: 39,
-  RIGHT: 37,
+  LEFT: 37,
+  RIGHT: 39
 };
 
-document.addEventListener("keyup", dibujarTeclado);
+console.log(teclas);
 
-function dibujarTeclado(evento) {
-  console.log(evento);
+document.addEventListener("keyup", dibujarTeclado);
+var cuadrito = document.getElementById("lienzo");
+var papel = cuadrito.getContext("2d");
+var x = 150;
+var y = 150;
+
+
+dibujarLinea("red",149,149,151,151,papel);
+
+function dibujarLinea(color, xinicial, yinicial, xfinal, yfinal, lienzo)
+{
+  lienzo.beginPath();
+  lienzo.strokeStyle = color;
+  lienzo.lineWidth = 3;
+  lienzo.moveTo(xinicial, yinicial);
+  lienzo.lineTo(xfinal, yfinal);
+  lienzo.stroke();
+  lienzo.closePath();
 }
 
-$(document).ready(function () {
-  // para debug prefiero console.log
-  //alert("Hola mundo");
-  //let varb=$('button').html();
-  //console.log(varb);
-  //cambia el contenido de la etiqueta button
-  //$('button').html('cambiado')
-});
+// la variable evento obtiene todo el detalle del evento
+function dibujarTeclado(evento)
+{
+  // if(evento.keyCode == teclas.UP)
+  // {
+  //   console.log("Vamo pa arriba");
+  // }
+  var colorcito = "blue";
+  var movimiento = 5;
+  switch (evento.keyCode) {
+    case teclas.UP:
+      dibujarLinea(colorcito,x,y,x,y-movimiento,papel)
+      y=y-movimiento;
+      break;
+    case teclas.DOWN:
+      dibujarLinea(colorcito,x,y,x,y+movimiento,papel)
+      y=y+movimiento;
+      break;
+    case teclas.LEFT:
+      dibujarLinea(colorcito,x,y,x-movimiento,y,papel)
+      x=x-movimiento;
+      break;
+    case teclas.RIGHT:
+      dibujarLinea(colorcito,x,y,x+movimiento,y,papel)
+      x=x+movimiento;
+      break;
+    default:
+      console.log("Otra letra");
+      break;
+
+  }
+
+}
